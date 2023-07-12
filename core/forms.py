f9c1be956b9fe2dd5dcfdd51c9723343e8909ab4
VictorOdongo <victorodongo397@gmail.com>
2023-07-12 16:40:34 +0300

    models

--- core/forms.py
+++ core/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
-from .models import Customer
 
 import re
 
@@ -23,18 +22,3 @@ class SignUpForm(UserCreationForm):
             raise ValidationError("The email already exists.")
         return email
     
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email'].lower()
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.save()
-
-        if commit:
-            customer, created = Customer.objects.get_or_create(user=user)
-            customer.email = user.email
-            customer.first_name = user.first_name
-            customer.last_name = user.last_name
-            customer.save()
-
-        return user
--- core/models.py
+++ core/models.py
@@ -17,8 +17,6 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to='customer/avatars/', blank=True, null=True)
     phone_number = models.CharField(max_length=50, blank=True)
-    paypal_email = models.EmailField(max_length=255, blank=False, default="2")
-
     
     def __str__(self):
         return self.user.get_full_name()
